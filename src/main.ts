3168cd68d47a990b3bc7fa9bac10290ba282d898
Md Wahiduzzaman Suva <wshuvo360@gmail.com>
2025-02-21 21:16:31 +0600

    deploying with vercel

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { VercelAdapter } from '@nestjs/platform-express';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: "http://localhost:3000", 
-    credentials: true, 
+async function createApp() {
+  const app = express();
+  const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(app));
+  nestApp.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
   });
-
-  await app.listen(process.env.PORT ?? 3001);
+  await nestApp.init();
+  return app;
 }
-bootstrap();
+
+// The function that will be used as a handler in Vercel
+export default async (req: VercelRequest, res: VercelResponse) => {
+  const app = await createApp();
+  app(req, res); // Use Express to handle the request
+};
